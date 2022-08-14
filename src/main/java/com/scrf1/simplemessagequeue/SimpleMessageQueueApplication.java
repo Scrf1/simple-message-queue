457ae8e32c760855e16c5b605b02f6c3415d3d7d
SCrf1 <srgseraphin14@gmail.com>
2022-08-14 15:57:32 +0200

    First commit

---
+++ src/main/java/com/scrf1/simplemessagequeue/SimpleMessageQueueApplication.java
@@ -0,0 +1,13 @@
+package com.scrf1.simplemessagequeue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMessageQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMessageQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrf1/simplemessagequeue/SimpleMessageQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrf1.simplemessagequeue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMessageQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
