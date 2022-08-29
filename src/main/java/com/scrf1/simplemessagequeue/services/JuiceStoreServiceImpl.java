34213deeea4a4675f1d3599f3302aab1be5f3f13
SCrf1 <srgseraphin14@gmail.com>
2022-08-29 23:10:12 +0200

    Adding database schema generator

--- src/main/java/com/scrf1/simplemessagequeue/services/JuiceStoreServiceImpl.java
+++ src/main/java/com/scrf1/simplemessagequeue/services/JuiceStoreServiceImpl.java
@@ -6,9 +6,11 @@ import com.scrf1.simplemessagequeue.models.JuiceStore;
 import com.scrf1.simplemessagequeue.repositories.JuiceRepository;
 import com.scrf1.simplemessagequeue.repositories.JuiceStoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class JuiceStoreServiceImpl implements JuiceStoreService {
 
     @Autowired
